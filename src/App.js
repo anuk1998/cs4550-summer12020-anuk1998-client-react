514708371dfa1cc7c5467cd97c373751c14785aa
Anu Kandasamy <kandasamy.a@husky.neu.edu>
2020-05-22 21:34:34 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import WhiteBoard from "./components/WhiteBoard"
+
+function App() {
+  return (
+   <WhiteBoard/>
+  );
+}
+
+export default App;
---
+++ src/components/CourseGridComponent.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default class CourseGridComponent extends React.Component {
+    render() {
+        return <h3>Course Grid</h3>
+    }
+}
